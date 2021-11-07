8fe8f535b51c0c79da7944895dcc4671ecbaba06
dayeondayeon <dayeon2438@gmail.com>
2021-11-07 14:39:17 +0900

    Add Post Router

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const accountRouter = require('./account'); //로그인 라우터
+const postRouter = require('./post');
 
 router.post('/signin', accountRouter); //로그인
 router.post('/signup', accountRouter); //회원가입
+router.post('/post', postRouter);
 module.exports = router;
---
+++ routes/post.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../modules/multer');
+const statusCode = require('../modules/status');
+const messageCode = require('../modules/message');
+const connection = require('../modules/mysql');
+
+router.post('/post', upload.single('image'), async(req, res) => {
+    const { writer, content, price, productname, min, current, ask, notice, image } = req.body;
+    console.log = req.file;
+    const img = req.file.path; //이미지 경로
+    if (!writer || !content || !price || !productname || !min || !current || !ask || !notice) {
+        return res.status(statusCode.BAD_REQUEST).send(messageCode.POST_FAIL);
+    }
+
+    const sql = `INSERT INTO Post(writer, content, price, product_name, min_requester, cur_requester, qna_link, notice_link, image) 
+    VALUES (${writer}, '${content}', ${price}, '${productname}', ${min}, ${current}, '${ask}', '${notice}', '${img}')`;
+    connection.query(sql, function(err, rows) {
+        var resultCode = statusCode.BAD_REQUEST;
+        var message = messageCode.POST_FAIL;
+        if (err) {
+            console.log(err);
+            return res.status(resultCode).send(message);
+        } else {
+            resultCode = statusCode.SUCCESS;
+            message = messageCode.POST_SUCCESS;
+        }
+
+        return res.status(resultCode).json({
+            code : resultCode,
+            message : message,
+        });
+    })
+})
+module.exports = router;
